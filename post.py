c7a35e6fa05f96f20cd51d736ee8e47c9ad55656
ScanEarth <63190798+ScanEarth@users.noreply.github.com>
2020-04-05 21:44:42 +0200

    Change to My Twitterpost

--- post.py
+++ post.py
@@ -12,7 +12,7 @@ ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
-argentinaFlag = u'\U0001F1E6' + u'\U0001F1F7'
+swedenFlag = u'\U0001F1F8' + u'\U0001F1EA'
 
 filenames = []
 for element in sys.argv[3:]:
@@ -23,4 +23,4 @@ for filename in filenames:
   res = api.media_upload(filename)
   media_ids.append(res.media_id)
 
-api.update_status(status=argentinaFlag + ' Imagen satelital: ' + sys.argv[1] + '. Elevacion maxima: ' + sys.argv[2] + ' grados. #NOAA #weather #argentinaimagenes #noaasatellite #clima #wxtoimg #raspberrypi #argentina #argentinasat', media_ids=media_ids)
+api.update_status(status=swedenFlag + ' Satellitbilder: ' + sys.argv[1] + '. Max elevering: ' + sys.argv[2] + ' grader. #NOAA #weather #noaasatellite #wxtoimg #raspberrypi #sverige #satellitbilder', media_ids=media_ids)
